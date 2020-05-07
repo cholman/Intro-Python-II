73a45c69fa13169011ba42d491f3af6113aabc8f
Cody Holman <holman.cody@gmail.com>
2020-05-07 16:46:45 -0500

    mvp2

--- src/adv.py
+++ src/adv.py
@@ -8,7 +8,7 @@ room = {
                      "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", ["rope", "coins"]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
@@ -38,35 +38,69 @@ room['treasure'].s_to = room['narrow']
 # Main
 #
 player = Player("player1", room['outside'])
+print(f'{player.current_room}')
+
 while True:
-    print(player)
-    action = input("What direction do you want to go? ")
-    if action == "q":
+    action = input("\nWhat would you like to do? ").split()
+    if action[0] in ("n", "s", "e", "w"):
+        if hasattr(player.current_room, f'{action[0]}_to'):
+            player.set_location(getattr(player.current_room, f'{action[0]}_to'))
+            # player.current_room = getattr(player.current_room, f'{action}_to')
+            print(f'Moved to {player.current_room.name}\n\n')
+            print(f'{player.current_room.description}')
+            print(f'You see {player.current_room.items}')
+        else:
+            print("You find nothing interesting\n\n")
+    # elif action[1] in player.current_room.items:
+        # player.getitems(player.current_room.items)
+    elif action[0] == "get":
+        if action[1] in player.current_room.items:
+            # player.getitems(action[1])
+            for item in player.current_room.items:
+                if item == action[1]:
+                    player.getitem(item)
+                    player.current_room.items.remove(item)
+                    print(f"You see {player.current_room.items}")
+    elif action[0] == "drop":
+        if action[1] in player.inventory:
+            for item in player.inventory:
+                if item == action[1]:
+                    player.dropitem(item)
+                    player.current_room.items.append(item)
+                    print(f'You drop {item} on the ground.')
+                    print(f"You see {player.current_room.items}")
+
+
+
+        
+    if action[0] == "q":
         exit()
-    if action == "n":
-        if player.room.n_to:
-            print('Moved North')
-            player.room = player.room.n_to
-        else:
-            print('Invalid Movement')
-    if action == "s":
-        if player.room.s_to:
-            print('Moved South')
-            player.room = player.room.s_to
-        else:
-            print('Invalid Movement')
-    if action == "e":
-        if player.room.e_to:
-            print('Moved East')
-            player.room = player.room.e_to
-        else:
-            print('Invalid Movement')
-    if action == "w":
-        if player.room.w_to:
-            print('Moved West')
-            player.room = player.room.w_to
-        else:
-            print('Invalid Movement')
+
+
+    # if action == "n":
+    #     if player.room.n_to:
+    #         print('Moved North')
+    #         player.room = player.room.n_to
+    #     else:
+    #         print('Invalid Movement')
+    # if action == "s":
+    #     if player.room.s_to:
+    #         print('Moved South')
+    #         player.room = player.room.s_to
+    #     else:
+    #         print('Invalid Movement')
+    # if action == "e":
+    #     if player.room.e_to:
+    #         print('Moved East')
+    #         player.room = player.room.e_to
+    #     else:
+    #         print('Invalid Movement')
+    # if action == "w":
+    #     if player.room.w_to:
+    #         print('Moved West')
+    #         player.room = player.room.w_to
+    #     else:
+    #         print('Invalid Movement')
 
 
 # Make a new player object that is currently in the 'outside' room.
--- src/player.py
+++ src/player.py
@@ -1,13 +1,21 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
-from room import Room
 class Player: 
-    def __init__(self, name, room):
+    def __init__(self, name, current_room, inventory = []):
         self.name = name
-        self.room = room
+        self.current_room = current_room
+        self.inventory = inventory
 
     def __str__(self):
-        return f'{self.name} is at {self.room}'
+        return f'{self.name}'
+
+    def set_location(self, current_room):
+        self.current_room = current_room
+
+    def getitem(self, item):
+        self.inventory.append(item)
+        print(f'You put {self.inventory} in your inventory.')
     
-    def action(self, room):
-        self.room = room
+    def dropitem(self, item):
+        self.inventory.remove(item)
+        
